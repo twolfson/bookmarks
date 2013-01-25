786c17fa853e5fb4ddc8cf564bc2afa4c5b24fb2
Todd Wolfson <todd@twolfson.com>
2013-01-25 02:01:21 -0800

    Outputting giant list. om nom nom.

--- public/js/index.js
+++ public/js/index.js
@@ -20,45 +20,41 @@ $(function () {
   // Grab our template
   var template = $('#bookmark-template').html();
 
-  // Mock bookmarks
-  var bookmarks = [
-    {
-        "title": "fxn/tkn - Terminal keynote presentation",
-        "dateAdded": 1349313664591754,
-        "lastModified": 1349313676308463,
-        "description": "tkn - Terminal Keynote - A hack for terminal-based talks",
-        "uri": "https://github.com/fxn/tkn"
-    },
-    {
-        "title": "pedalboard.js - Open-source JavaScript framework for developing audio effects",
-        "dateAdded": 1349417966733927,
-        "lastModified": 1349417969037920,
-        "description": "pedalboard.js - Open-source JavaScript framework for developing audio e",
-        "uri": "http://dashersw.github.com/pedalboard.js/demo/"
-    }
-  ];
+  // Load in our bookmarks
+  $.getJSON('bookmarks.min.json', function (bookmarks, success, res) {
+    // // Mock bookmarks
+    // var bookmarks = [
+    //   {
+    //       "title": "fxn/tkn - Terminal keynote presentation",
+    //       "dateAdded": 1349313664591754,
+    //       "lastModified": 1349313676308463,
+    //       "description": "tkn - Terminal Keynote - A hack for terminal-based talks",
+    //       "uri": "https://github.com/fxn/tkn"
+    //   },
+    //   {
+    //       "title": "pedalboard.js - Open-source JavaScript framework for developing audio effects",
+    //       "dateAdded": 1349417966733927,
+    //       "lastModified": 1349417969037920,
+    //       "description": "pedalboard.js - Open-source JavaScript framework for developing audio e",
+    //       "uri": "http://dashersw.github.com/pedalboard.js/demo/"
+    //   }
+    // ];
 
-  // Iterate over the bookmarks
-  var bookmarkHtmlArr = bookmarks.map(function (bookmark) {
-        var html = templatez(template, bookmark);
-        return html;
-      }),
-      bookmarkHtml = bookmarkHtmlArr.join('');
+    // Iterate over the bookmarks
+    var bookmarkHtmlArr = bookmarks.map(function (bookmark) {
+          var html = templatez(template, bookmark);
+          return html;
+        }),
+        bookmarkHtml = bookmarkHtmlArr.join('');
 
-  // Append the content to our table
-  var bookmarkList = document.getElementById('bookmark-list');
-  bookmarkList.innerHTML = bookmarkHtml;
+    // Append the content to our table
+    var bookmarkList = document.getElementById('bookmark-list');
+    bookmarkList.innerHTML = bookmarkHtml;
 
-  // Performify the list
-  var options = {
-        valueNames: ['description', 'uri']
-      },
-      list = new List('bookmark-list-container', options);
-
-  // // Load in our bookmarks
-  // $.getJSON('bookmarks.min.json', function (bookmarks, success, res) {
-  //   bookmarks.forEach(function (bookmark) {
-  //     list.add(bookmark);
-  //   });
-  // });
+    // Performify the list
+    var options = {
+          valueNames: ['description', 'uri']
+        },
+        list = new List('bookmark-list-container', options);
+  });
 });
