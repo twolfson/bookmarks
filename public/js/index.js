1fbcebdacfe075b94d52a5d3fe04fba041509f55
Todd Wolfson <todd@twolfson.com>
2013-01-25 01:29:34 -0800

    Dealing with list.js

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+// When the DOM is ready
+$(function () {
+  // Create our List
+  // var ul = document.createElement('ul'),
+  var ul = document.getElementById('list'),
+      options = {
+        valueNames: ['description', 'uri']
+      },
+      list = new List(ul, options);
+
+  // Append the list to main
+  var $main = $('#main');
+  $main.append(ul);
+
+  // Add in a few bookmarks
+  var bookmarks = [
+    {
+        "title": "fxn/tkn - Terminal keynote presentation",
+        "dateAdded": 1349313664591754,
+        "lastModified": 1349313676308463,
+        "description": "tkn - Terminal Keynote - A hack for terminal-based talks",
+        "uri": "https://github.com/fxn/tkn"
+    },
+    {
+        "title": "pedalboard.js - Open-source JavaScript framework for developing audio effects for guitars",
+        "dateAdded": 1349417966733927,
+        "lastModified": 1349417969037920,
+        "description": "pedalboard.js - Open-source JavaScript framework for developing audio effects for guitars",
+        "uri": "http://dashersw.github.com/pedalboard.js/demo/"
+    }
+  ];
+  // list.add(bookmarks[0]);
+  // list.add(bookmarks[1]);
+});
