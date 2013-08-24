8b27e877799475a4695303db1fe51ad11c29a0eb
Todd Wolfson <todd@twolfson.com>
2013-08-24 16:49:35 -0700

    Starting to test nested bookmarks

--- test/bookmarks_test.js
+++ test/bookmarks_test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 
 // Basic test
 describe('bookmarks', function () {
-  describe('formatting a simple set of bookmarks', function () {
+  describe.skip('formatting a simple set of bookmarks', function () {
     before(function () {
       // Prepare globals for bookmarks
       global.bookmarks = require('./test_files/simple.input.json');
@@ -19,6 +19,29 @@ describe('bookmarks', function () {
       var actualBookmarks = require(global.output),
           expectedBookmarks = require('./expected_files/simple.output.json');
       assert.deepEqual(actualBookmarks, expectedBookmarks);
+    });
+  });
+
+  describe('formatting a nested set of bookmarks', function () {
+    before(function () {
+      // Prepare globals for bookmarks
+      var bookmarks = require('../bookmarks.orig.json');
+      console.log(JSON.stringify(bookmarks, function (key, val) {
+        console.log(arguments);
+        return undefined;
+      }, 4));
+      // global.config = require('./test_files/simple.config.json');
+      // global.output = __dirname + '/actual_files/simple.output.json';
+
+      // // Load our bookmarks script
+      // var bookmarks = require('../lib/bookmarks.js');
+    });
+
+    it('returns an matching array of bookmarks', function () {
+    //   // Compare actual output to expected output
+    //   var actualBookmarks = require(global.output),
+    //       expectedBookmarks = require('./expected_files/simple.output.json');
+    //   assert.deepEqual(actualBookmarks, expectedBookmarks);
     });
   });
 });
