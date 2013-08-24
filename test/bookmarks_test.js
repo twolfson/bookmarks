233edddc2638ff6f65a085b2167a9f810b5d60dc
Todd Wolfson <todd@twolfson.com>
2013-08-24 16:24:18 -0700

    Completed first test

--- test/bookmarks_test.js
+++ test/bookmarks_test.js
@@ -1,17 +1,24 @@
+// Load in assert
+var assert = require('assert');
+
+// Basic test
 describe('bookmarks', function () {
   describe('formatting a simple set of bookmarks', function () {
     before(function () {
       // Prepare globals for bookmarks
-      global.bookmarks = require('./test_files/test_filessimple.input.json');
-      global.config = require('./test_files/test_filessimple.config.json');
-      global.output = require(__dirname + '/test_files/actual_files/simple.output.json');
+      global.bookmarks = require('./test_files/simple.input.json');
+      global.config = require('./test_files/simple.config.json');
+      global.output = __dirname + '/actual_files/simple.output.json';
 
       // Load our bookmarks script
       var bookmarks = require('../lib/bookmarks.js');
     });
 
     it('returns an matching array of bookmarks', function () {
-      // Read
+      // Compare actual output to expected output
+      var actualBookmarks = require(global.output),
+          expectedBookmarks = require('./expected_files/simple.output.json');
+      assert.deepEqual(actualBookmarks, expectedBookmarks);
     });
   });
 });
