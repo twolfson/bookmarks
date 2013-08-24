69b9c9611f82bcb58a883a2e086850696a6bd08f
Todd Wolfson <todd@twolfson.com>
2013-08-24 16:19:13 -0700

    Opening up script for testing and writing first test

--- test/bookmarks_test.js
+++ test/bookmarks_test.js
@@ -1,11 +1,17 @@
-var bookmarks = require('../lib/bookmarks.js');
-
 describe('bookmarks', function () {
-  before(function () {
+  describe('formatting a simple set of bookmarks', function () {
+    before(function () {
+      // Prepare globals for bookmarks
+      global.bookmarks = require('./test_files/test_filessimple.input.json');
+      global.config = require('./test_files/test_filessimple.config.json');
+      global.output = require(__dirname + '/test_files/actual_files/simple.output.json');
 
-  });
+      // Load our bookmarks script
+      var bookmarks = require('../lib/bookmarks.js');
+    });
 
-  it('', function () {
-
+    it('returns an matching array of bookmarks', function () {
+      // Read
+    });
   });
 });
