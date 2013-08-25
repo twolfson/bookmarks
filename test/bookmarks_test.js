f18f12276ff557354fd47896276231765b163c89
Todd Wolfson <todd@twolfson.com>
2013-08-24 17:21:24 -0700

    Found out that we are generating value for folders

--- test/bookmarks_test.js
+++ test/bookmarks_test.js
@@ -12,6 +12,7 @@ describe('bookmarks', function () {
 
       // Parse the bookmarks
       this.output = parser(input, options);
+      // require('grunt').file.write(__dirname + '/actual_files/simple.output.json', JSON.stringify(this.output, null, 4));
     });
 
     it('returns an matching array of bookmarks', function () {
@@ -25,33 +26,19 @@ describe('bookmarks', function () {
   describe('formatting a nested set of bookmarks', function () {
     before(function () {
       // Prepare globals for bookmarks
-      var bookmarks = require('../bookmarks.orig.json');
-      console.log(JSON.stringify(bookmarks, function (key, val) {
-        // If the key is root or children, return it
-        if (key === '' || key === 'children') {
-          return val;
-        }
+      var input = require('./test_files/nested.input.json'),
+          options = require('./test_files/nested.config.json');
 
-        // If this is an array, slice it down
-        if (Array.isArray(val)) {
-          console.log(key, val.slice(0, 10));
-        }
-
-        // Otherwise, return undefined
-        return undefined;
-      }, 4));
-      // global.config = require('./test_files/simple.config.json');
-      // global.output = __dirname + '/actual_files/simple.output.json';
-
-      // // Load our bookmarks script
-      // var bookmarks = require('../lib/bookmarks.js');
+      // Parse the bookmarks
+      this.output = parser(input, options);
+      require('grunt').file.write(__dirname + '/actual_files/nested.output.json', JSON.stringify(this.output, null, 4));
     });
 
     it('returns an matching array of bookmarks', function () {
-    //   // Compare actual output to expected output
-    //   var actualBookmarks = require(global.output),
-    //       expectedBookmarks = require('./expected_files/simple.output.json');
-    //   assert.deepEqual(actualBookmarks, expectedBookmarks);
+      // Compare actual output to expected output
+      var actualBookmarks = this.output,
+          expectedBookmarks = require('./expected_files/nested.output.json');
+      assert.deepEqual(actualBookmarks, expectedBookmarks);
     });
   });
 });
