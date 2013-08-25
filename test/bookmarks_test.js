c783bb2031543182597d0d478eb5a61312326f88
Todd Wolfson <todd@twolfson.com>
2013-08-24 17:03:13 -0700

    Broke out bin logic. Much more testable

--- test/bookmarks_test.js
+++ test/bookmarks_test.js
@@ -1,22 +1,22 @@
 // Load in assert
-var assert = require('assert');
+var assert = require('assert'),
+    parser = require('../lib/bookmarks');
 
 // Basic test
 describe('bookmarks', function () {
-  describe.skip('formatting a simple set of bookmarks', function () {
+  describe('formatting a simple set of bookmarks', function () {
     before(function () {
       // Prepare globals for bookmarks
-      global.bookmarks = require('./test_files/simple.input.json');
-      global.config = require('./test_files/simple.config.json');
-      global.output = __dirname + '/actual_files/simple.output.json';
+      var input = require('./test_files/simple.input.json'),
+          options = require('./test_files/simple.config.json');
 
-      // Load our bookmarks script
-      var bookmarks = require('../lib/bookmarks.js');
+      // Parse the bookmarks
+      this.output = parser(input, options);
     });
 
     it('returns an matching array of bookmarks', function () {
       // Compare actual output to expected output
-      var actualBookmarks = require(global.output),
+      var actualBookmarks = this.output,
           expectedBookmarks = require('./expected_files/simple.output.json');
       assert.deepEqual(actualBookmarks, expectedBookmarks);
     });
